31311c4aa45e628c33045d1fdec1ca52d5232ab7
raul <backtobisnez@yandex.ru>
2024-06-28 17:05:14 +0500

    start tgapi

--- src/App.js
+++ src/App.js
@@ -2,33 +2,91 @@ import decoHeader from "./img/deco-header.svg";
 import underlineHeader from "./img/underline-header.svg";
 import "./App.css";
 import StoreItem from "./components/StoreItem";
+import { useEffect } from "react";
+const tg = window.Telegram.WebApp;
 
 function App() {
+  let btns = document.querySelectorAll(".store-button");
+  let cartItems = new Set();
+
+  useEffect(() => {
+    tg.expand();
+    tg.MainButton.hide();
+    tg.ready();
+  }, []);
+
+  const OnClose = () => {
+    tg.Close();
+  };
+
+  const OnToggleButton = () => {
+    if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  };
+
+  for (let btn of btns) {
+    btn.onclick = () => {
+      let name = btn.getAttribute("data-name");
+      let img = btn.querySelector("img");
+      OnToggleButton();
+      if (cartItems.has(name)) {
+        cartItems.delete(name);
+        btn.style.background = "none";
+        img.style.transform = "rotate(1turn)";
+      } else {
+        cartItems.add(name);
+        btn.style.background = "#D9D9D9";
+        img.style.transform = "rotate(-1turn)";
+      }
+      console.log(cartItems);
+    };
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          WALKN
-        </h1>
+        <h1>WALKN</h1>
         <p>от англ. walking - гуляющий</p>
       </header>
       <main>
-      <div class={"menu"}>
-        <button href={"#"}>ОБУВЬ</button>
-        <button href={"#"}>ОДЕЖДА</button>
-        <button href={"#"}>О НАС</button>
-      </div>
+        <div class={"menu"}>
+          <button href={"#"} class={"menu-button"}>
+            ОБУВЬ
+          </button>
+          <button href={"#"} class={"menu-button"}>
+            ОДЕЖДА
+          </button>
+          <button href={"#"} class={"menu-button"}>
+            О НАС
+          </button>
+        </div>
 
-      <div class={"store"}>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-        <StoreItem info={{ Price: 5500, Name: "крутой ботинок" }}></StoreItem>
-      </div>
+        <div class={"store"}>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок1" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок2" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок3" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок4" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок5" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок6" }}
+          ></StoreItem>
+          <StoreItem
+            info={{ Price: 5500, Name: "крутой ботинок7" }}
+          ></StoreItem>
+        </div>
       </main>
     </div>
   );
--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import cart from "../img/cart-plus-svgrepo-com.svg";
 
 const StoreItem = function (props) {
   return (
     <div class={"store-item"}>
-      <img src={"https://www.military1st.ru/media/catalog/product/cache/370473b00da80d51dea07108e3e83e0d/s/u/surplus_undercover_stiefel_8inch_boots_BLK_1.jpg"}/>
+      <img
+        src={
+          "https://www.military1st.ru/media/catalog/product/cache/370473b00da80d51dea07108e3e83e0d/s/u/surplus_undercover_stiefel_8inch_boots_BLK_1.jpg"
+        }
+      />
       <h3>{props.info.Name}</h3>
-      <p>{props.info.Price}</p>
+      <div class={"price-container"}>
+        <p>{props.info.Price}</p>
+        <button
+          class={"store-button"}
+          data-name={props.info.Name}
+          data-check={false}
+        >
+          <img src={cart} />
+        </button>
+      </div>
     </div>
   );
 };
